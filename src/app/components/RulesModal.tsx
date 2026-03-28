044c35ea82e0bcf740ec5fef8b86c56e52f9b47d
Abhishek Jha <abhishekjha3012@gmail.com>
2026-03-28 18:21:01 +0530

    Moved rules to constant file

--- src/app/components/RulesModal.tsx
+++ src/app/components/RulesModal.tsx
@@ -1,5 +1,6 @@
-import { X, Info, Trophy } from 'lucide-react';
-import { useState } from 'react';
+import { X, Trophy } from 'lucide-react';
+
+import { rules } from '../data/rules';
 
 interface RulesModalProps {
   isOpen: boolean;
@@ -8,38 +9,6 @@ interface RulesModalProps {
 
 export function RulesModal({ isOpen, onClose }: RulesModalProps) {
   if (!isOpen) return null;
-
-  const rules = [
-    "1. Minimum 30 overs of game has to happen.",
-
-    "2. The match must have a result. (App should not indicate that the match" +
-    " was abandoned).",
-
-    "3. Big Win Rule" +
-    "\nIf Rank 1 wins by more than 100 points, Winner takes all the prize money.",
-
-    "4. Tie Rule" +
-    "\nIf Rank 1 and Rank 2 are tied, their winning amounts will be combined and" +
-    "shared equally. The winning amount for Rank 3 will remain unchanged.",
-
-    "5. Tie Rule" +
-    "\nIf Rank 2 and Rank 3 are tied, their winning amounts will be combined and" +
-    "shared equally. The winning amount for Rank 1 will remain unchanged.",
-
-    "6. Minimum Matches to Play" + 
-    "\nLeague stage → 50 matches" + 
-    "\nPlayoffs → 2 matches" +
-    "\nFinal → 1 match " + 
-    "\nThese are compulsory (no exceptions)",
-
-    "7. Entry Fee (per match) " +
-    "\nLeague stage → 50" + 
-    "\nPlayoffs → ₹100" +
-    "\nFinal → ₹200 ",
-
-    "8. Submission Deadline" +
-      "\nYou must join/share your team in WhatsApp group before first bowl bowled."
-  ];
 
   return (
     <>
